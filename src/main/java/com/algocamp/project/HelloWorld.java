1e9978cb7d658579d7fa3ecc23fbf3d7833db9b0
Chetan <chetanrjaiswal77@gmail.com>
2024-04-29 15:52:05 +0530

    This is commit related to POC for bazel build tool

---
+++ src/main/java/com/algocamp/project/HelloWorld.java
@@ -0,0 +1,10 @@
+package main.java.com.algocamp.project;
+
+import main.java.com.algocamp.project.utils.MyUtil;
+
+public class HelloWorld{
+    public static void main(String []args){
+        System.out.println("Hello chetan jaiswal");
+        MyUtil.getString();
+    }
+}
---
+++ src/main/java/com/algocamp/project/utils/MyUtil.java
@@ -0,0 +1,8 @@
+package main.java.com.algocamp.project.utils;
+
+public class MyUtil {
+
+    public static void getString(){
+        System.out.println("some string");
+    }
+} 
